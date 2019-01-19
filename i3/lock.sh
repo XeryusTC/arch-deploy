aed04720c8eab7d9a9d6484e5083d6c46d26bad3
Xeryus Stokkel <armadillo@onenetbeyond.org>
2019-01-19 22:21:17 +0100

    Use maim instead of scrot for the lock screen

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Take screenshot
-scrot /tmp/lockscreen.png
+maim /tmp/lockscreen.png
 # Pixellate to 10x
 convert "/tmp/lockscreen.png" -blur 0x3 "/tmp/lockscreen2.png"
 # Lock screen displaying the image
